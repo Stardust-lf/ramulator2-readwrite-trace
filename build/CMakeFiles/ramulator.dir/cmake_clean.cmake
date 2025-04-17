87212b5980c5ac4e7d68deaf57171d704253d199
Fan Lee <frozenstar200101@gmail.com>
2025-04-17 19:25:44 -0400

    build

---
+++ build/CMakeFiles/ramulator.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/fann/projects/ramulator2-readwrite-trace/libramulator.pdb"
+  "/home/fann/projects/ramulator2-readwrite-trace/libramulator.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ramulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
